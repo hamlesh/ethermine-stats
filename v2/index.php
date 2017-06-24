be8c379f433dfbae2d7f6ec7d5741ccecd4606a9
Simon Mott <simon@simonmott.co.uk>
2017-06-25 00:33:51 +0100

    dont use php short tags

--- v2/index.php
+++ v2/index.php
@@ -13,7 +13,7 @@
 			<h1>MIN&Xi;</h1>
 		</div>
 
-		<? if ( $stat['waiting'] == '1' ) {
+		<?php if ( $stat['waiting'] == '1' ) {
 			echo '<div class="col-md-12"><p align="center"><em>There is insufficient data to produce any useful metrics.<br>Please check your wallet settings in config.php.<br>The pool you are querying may also be limiting API requests - please try later.</em></p></div>';
 			die;
 		} ?>
@@ -22,8 +22,8 @@
 			<ul class="list-group">
 				<li class="list-group-item list-group-item-<?=$conf['colour']?>"><h4>Statistics</h4></li>
 				<li class="list-group-item">
-					<? if ( $conf['show_reportedhash'] == '0' ) { ?>Mined<span class="pull-right">&Xi;<?=$stat['unpaid']?></span> <? } ?>
-					<? if ( $conf['show_reportedhash'] == '1' ) { ?>Reported Hashrate<span class="pull-right"><?=$stat['reportedhashrate']?> MH/s</span> <? } ?>
+					<?php if ( $conf['show_reportedhash'] == '0' ) { ?>Mined<span class="pull-right">&Xi;<?=$stat['unpaid']?></span> <?php } ?>
+					<?php if ( $conf['show_reportedhash'] == '1' ) { ?>Reported Hashrate<span class="pull-right"><?=$stat['reportedhashrate']?> MH/s</span> <?php } ?>
 				</li>				
 				<li class="list-group-item">Effective Hashrate [60 mins]	<span class="pull-right"><?=$stat['hashrate']?></span></li>
 				<li class="list-group-item">Average Hashrate [24 hrs]		<span class="pull-right"><?=$stat['avghashrate']?> MH/s</span></li>
@@ -39,7 +39,7 @@
 			</ul>
 		</div>
 
-		<? if ( $conf['show_bar'] == '1' ) { ?>
+		<?php if ( $conf['show_bar'] == '1' ) { ?>
 		<div class="col-md-12">
 			<div class="progress">
 			  	<div class="progress-bar progress-bar-striped progress-bar-<?=$conf['colour']?> active" role="progressbar" aria-valuenow="<?=$stat['unpaid']?>" aria-valuemin="0" aria-valuemax="100" style="width:<?=number_format($stat['unpaid']*100)?>%">
@@ -47,56 +47,56 @@
 				</div>
 			</div><br>
 		</div>
-		<? } ?>	
+		<?php } ?>	
 
 		<div class="col-md-3">
 			<ul class="list-group">
 				<li class="list-group-item list-group-item-<?=$conf['colour']?>"><h4>&Xi;TH</h4></li>
-				<? if ( $conf['show_min'] == '1' ) { ?>		<li class="list-group-item">Minute 	<span class="pull-right">&Xi;<?=number_format($stat['emin'],5)?></span></li>	<? } ?>
-				<? if ( $conf['show_hour'] == '1' ) { ?>	<li class="list-group-item">Hour 	<span class="pull-right">&Xi;<?=number_format($stat['ehour'],5)?></span></li>	<? } ?>
-				<? if ( $conf['show_day'] == '1' ) { ?>		<li class="list-group-item">Day 	<span class="pull-right">&Xi;<?=number_format($stat['eday'],5)?></span></li>	<? } ?>
-				<? if ( $conf['show_week'] == '1' ) { ?>	<li class="list-group-item">Week 	<span class="pull-right">&Xi;<?=number_format($stat['eweek'],5)?></span></li>	<? } ?>
-				<? if ( $conf['show_month'] == '1' ) { ?>	<li class="list-group-item">Month 	<span class="pull-right">&Xi;<?=number_format($stat['emonth'],5)?></span></li>	<? } ?>
+				<?php if ( $conf['show_min'] == '1' ) { ?>		<li class="list-group-item">Minute 	<span class="pull-right">&Xi;<?=number_format($stat['emin'],5)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_hour'] == '1' ) { ?>	<li class="list-group-item">Hour 	<span class="pull-right">&Xi;<?=number_format($stat['ehour'],5)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_day'] == '1' ) { ?>		<li class="list-group-item">Day 	<span class="pull-right">&Xi;<?=number_format($stat['eday'],5)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_week'] == '1' ) { ?>	<li class="list-group-item">Week 	<span class="pull-right">&Xi;<?=number_format($stat['eweek'],5)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_month'] == '1' ) { ?>	<li class="list-group-item">Month 	<span class="pull-right">&Xi;<?=number_format($stat['emonth'],5)?></span></li>	<?php } ?>
 			</ul>
 		</div>
 
 		<div class="col-md-3">
 			<ul class="list-group">
 				<li class="list-group-item list-group-item-<?=$conf['colour']?>"><h4>&Xi;TH -> <?=$fiat['code']?></h4></li>
-				<? if ( $conf['show_min'] == '1' ) { ?>		<li class="list-group-item">Minute 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['emin']*$ethtofiat),2)?></span></li>	<? } ?>
-				<? if ( $conf['show_hour'] == '1' ) { ?>	<li class="list-group-item">Hour 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['ehour']*$ethtofiat),2)?></span></li>	<? } ?>
-				<? if ( $conf['show_day'] == '1' ) { ?>		<li class="list-group-item">Day 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['eday']*$ethtofiat),2)?></span></li>	<? } ?>
-				<? if ( $conf['show_week'] == '1' ) { ?>	<li class="list-group-item">Week 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['eweek']*$ethtofiat),2)?></span></li>	<? } ?>
-				<? if ( $conf['show_month'] == '1' ) { ?>	<li class="list-group-item">Month 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['emonth']*$ethtofiat),2)?></span></li>	<? } ?>
+				<?php if ( $conf['show_min'] == '1' ) { ?>		<li class="list-group-item">Minute 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['emin']*$ethtofiat),2)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_hour'] == '1' ) { ?>	<li class="list-group-item">Hour 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['ehour']*$ethtofiat),2)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_day'] == '1' ) { ?>		<li class="list-group-item">Day 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['eday']*$ethtofiat),2)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_week'] == '1' ) { ?>	<li class="list-group-item">Week 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['eweek']*$ethtofiat),2)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_month'] == '1' ) { ?>	<li class="list-group-item">Month 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['emonth']*$ethtofiat),2)?></span></li>	<?php } ?>
 			</ul>
 		</div>
 
 		<div class="col-md-3">
 			<ul class="list-group">
 				<li class="list-group-item list-group-item-<?=$conf['colour']?>"><h4>&Xi;TH -> ฿TC</h4></li>
-				<? if ( $conf['show_min'] == '1' ) { ?>		<li class="list-group-item">Minute 	<span class="pull-right">฿<?=number_format($stat['bmin'],5)?></span></li>	<? } ?>
-				<? if ( $conf['show_hour'] == '1' ) { ?>	<li class="list-group-item">Hour 	<span class="pull-right">฿<?=number_format($stat['bhour'],5)?></span></li>	<? } ?>
-				<? if ( $conf['show_day'] == '1' ) { ?>		<li class="list-group-item">Day 	<span class="pull-right">฿<?=number_format($stat['bday'],5)?></span></li>	<? } ?>
-				<? if ( $conf['show_week'] == '1' ) { ?>	<li class="list-group-item">Week 	<span class="pull-right">฿<?=number_format($stat['bweek'],5)?></span></li>	<? } ?>
-				<? if ( $conf['show_month'] == '1' ) { ?>	<li class="list-group-item">Month 	<span class="pull-right">฿<?=number_format($stat['bmonth'],5)?></span></li>	<? } ?>
+				<?php if ( $conf['show_min'] == '1' ) { ?>		<li class="list-group-item">Minute 	<span class="pull-right">฿<?=number_format($stat['bmin'],5)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_hour'] == '1' ) { ?>	<li class="list-group-item">Hour 	<span class="pull-right">฿<?=number_format($stat['bhour'],5)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_day'] == '1' ) { ?>		<li class="list-group-item">Day 	<span class="pull-right">฿<?=number_format($stat['bday'],5)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_week'] == '1' ) { ?>	<li class="list-group-item">Week 	<span class="pull-right">฿<?=number_format($stat['bweek'],5)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_month'] == '1' ) { ?>	<li class="list-group-item">Month 	<span class="pull-right">฿<?=number_format($stat['bmonth'],5)?></span></li>	<?php } ?>
 			</ul>
 		</div>
 
 		<div class="col-md-3">
 			<ul class="list-group">
 				<li class="list-group-item list-group-item-<?=$conf['colour']?>"><h4>฿TC -> <?=$fiat['code']?></h4></li>
-				<? if ( $conf['show_min'] == '1' ) { ?>		<li class="list-group-item">Minute 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['bmin']*$btctofiat),2)?></span></li>	<? } ?>
-				<? if ( $conf['show_hour'] == '1' ) { ?>	<li class="list-group-item">Hour 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['bhour']*$btctofiat),2)?></span></li>	<? } ?>
-				<? if ( $conf['show_day'] == '1' ) { ?>		<li class="list-group-item">Day 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['bday']*$btctofiat),2)?></span></li>	<? } ?>
-				<? if ( $conf['show_week'] == '1' ) { ?>	<li class="list-group-item">Week 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['bweek']*$btctofiat),2)?></span></li>	<? } ?>
-				<? if ( $conf['show_month'] == '1' ) { ?>	<li class="list-group-item">Month 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['bmonth']*$btctofiat),2)?></span></li>	<? } ?>
+				<?php if ( $conf['show_min'] == '1' ) { ?>		<li class="list-group-item">Minute 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['bmin']*$btctofiat),2)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_hour'] == '1' ) { ?>	<li class="list-group-item">Hour 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['bhour']*$btctofiat),2)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_day'] == '1' ) { ?>		<li class="list-group-item">Day 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['bday']*$btctofiat),2)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_week'] == '1' ) { ?>	<li class="list-group-item">Week 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['bweek']*$btctofiat),2)?></span></li>	<?php } ?>
+				<?php if ( $conf['show_month'] == '1' ) { ?>	<li class="list-group-item">Month 	<span class="pull-right"><?=$fiat['sym']?><?=number_format(($stat['bmonth']*$btctofiat),2)?></span></li>	<?php } ?>
 			</ul>
 		</div>
 
 		<div class="col-md-12">
 			<ul class="list-group">
-				<!-- <? if ( $cache == '0' ) { ?><li class="list-group-item list-group-item-success">Live Data</li><? } ?> -->
-				<? if ( $cache == '1' ) { ?><li class="list-group-item list-group-item-warning">Using Cached Data: Too many requests to the Ethermine API</li><? } ?>
+				<!-- <?php if ( $cache == '0' ) { ?><li class="list-group-item list-group-item-success">Live Data</li><?php } ?> -->
+				<?php if ( $cache == '1' ) { ?><li class="list-group-item list-group-item-warning">Using Cached Data: Too many requests to the Ethermine API</li><?php } ?>
 			</ul>
 		</div>
 
